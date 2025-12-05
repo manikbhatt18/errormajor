b0ef0e8b2fc5a35e6ae8bc4bf6f7302f58385196
Manik Bhatt <bhattmanik94@gmail.com>
2025-12-05 17:48:44 +0530

    added font styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,34 @@
-import { Open_Sans } from "next/font/google";
+// import { Open_Sans } from "next/font/google";
+// import "./globals.css";
+
+// const openSans = Open_Sans({
+//   subsets: ["latin"],
+//   weight: ["400", "500", "600", "700"],
+//   variable: "--font-sans",
+// });
+
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <html lang="en">
+//       <body className={openSans.variable}>{children}</body>
+//     </html>
+//   );
+// }
+"use client";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const openSans = Open_Sans({
+const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  weight: ["400", "500", "600", "700"], // you can adjust weights as needed
   variable: "--font-sans",
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={openSans.variable}>{children}</body>
+      <body className={poppins.variable}>{children}</body>
     </html>
   );
 }
+
